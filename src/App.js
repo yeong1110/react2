e2ef87501a154483f4c6a720e09dbc89e4a3c41b
jeongyeongeun <a0257942@gmail.com>
2023-04-26 16:25:09 +0900

    v_1.5

--- src/App.js
+++ src/App.js
@@ -2,20 +2,26 @@
 import { useState } from 'react';
 import {Navbar, Container, Nav, Form} from 'react-bootstrap';
 import {Routes, Route, Link, useNavigate, Outlet} from 'react-router-dom'; 
+import styled from "styled-components";
 import './App.css';
 import img_01 from './banner.webp'
 import data from './data.js'
 import List from './Component/List.js'
 import Detail from './Pages/Detail.js'
 import About from './Pages/About.js'
+import axios from 'axios';
 
+
+let count = 0;
 
 function App() {
   let [items, setItems] = useState(data);
   let navigate = useNavigate();
   return (
     <div className="App">
+
             <Navbar bg="dark" variant="dark">
+
         <Container>
           <Navbar.Brand href="#home">Navbar</Navbar.Brand>
           <Nav className="me-auto">
@@ -47,6 +53,19 @@ function App() {
             }
           </div>
         </div>
+        {
+          count <1 ?  
+          <button onClick={()=>{
+            count = count +1;
+            axios.get('https://6c3fac62-d531-4787-a6d9-e01d76badde1.mock.pstmn.io/productList')
+            .then((result)=>{
+              // console.log(result.data)
+              let copyItems = [...items, ...result.data]
+              setItems(copyItems)
+            }).catch(()=>{console.log("통신 실패")})
+          }}>More</button>: null
+        }
+
         </>
       } />
       <Route path='*' element={<div>404Error</div>}/>
--- src/Pages/Detail.js
+++ src/Pages/Detail.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { useEffect } from 'react';
+
+
 
 const Detail = (props) => {
     let {id} = useParams();
+    let [alerts,setAlerts] = useState(true);
+    let [num, setNum] = useState(0);
     //유저가 url파라미터에 입력한 정보를 가져옴,작명했던 이름을 그대로 가져와야 정보를 받아올 수 있어(id),인덱싱 대신에 정보를 넣을 수도
+    
+    useEffect(()=>{
+        console.log("생명주기")
+        setTimeout(()=>{
+            setAlerts(false);
+        },3000)
+
+        //마운트 ,업데이트 둘 다 동시에 실행
+    })
+    useEffect(()=>{
+        // isNaN 으로 숫자인지 문자인지 구별하기 위해 쓴 것
+        if(isNaN(num) == true){
+            alert('숫자를 입력하세요');
+        }
+    },[num])
+
+
     return (
         <div className='container'>
+            {
+                alerts === true ? <div id='pop' className='alert alert-warning'>3초후 사라짐</div> : null
+            }
+            
             <div className='row'>
                 <div className='col-md-6'>
                     <img src={process.env.PUBLIC_URL + '/img/img0'+props.items[id].id+'.webp'} alt="" />
@@ -14,6 +40,10 @@ const Detail = (props) => {
                     <h4 className='pt-5'>{props.items[id].title}</h4>
                     <p>{props.items[id].content}</p>
                     <p>{props.items[id].price}원</p>
+                    <p>수량 <input type="text" onChange={(e)=>{
+                        setNum(e.target.value)
+                        
+                    }}/></p>
                     <button className='btn btn-primary'>주문하기</button>
                 </div>
             </div>
